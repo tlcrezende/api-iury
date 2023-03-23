bd5609f06b49b4e18f970273caa44fa1800d8966
Eduardo <eduardorodriguesanjos@gmail.com>
2023-03-23 13:15:37 -0300

    [fix] sei la

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get '/user', to: 'user#index'
-  get '/user/:id', to: 'user#show'
-  post '/user', to: 'user#update'
+  get '/users', to: 'user#index'
+  get '/users/:id', to: 'user#show'
+  post '/users', to: 'user#create'
+  patch '/users/:id', to: 'user#update'
+  put '/users/:id', to: 'user#update'
 
-  get '/turma', to: 'turma#index'
-  get '/turma/:id', to: 'turma#show'
-  post '/turma', to: 'turma#create'
+  get '/turmas', to: 'turma#index'
+  get '/turmas/:id', to: 'turma#show'
+  post '/turmas', to: 'turma#create'
 
   # Defines the root path route ("/")
   # root "articles#index"
