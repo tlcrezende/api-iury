5916034b7820457b923d111bc6b5b5a9ee2625f5
Thiago <tlcrezende@gmail.com>
2023-03-23 19:16:00 -0300

    some fixs

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
+  resources :turma_alunos
   mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :pagamentos
   get '/users', to: 'user#index'
   get '/users/:id', to: 'user#show'
   post '/users', to: 'user#create'
   patch '/users/:id', to: 'user#update'
-  put '/users/:id', to: 'user#update'
 
   get '/turmas', to: 'turma#index'
   get '/turmas/:id', to: 'turma#show'
   post '/turmas', to: 'turma#create'
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  patch '/turmas/:id', to: 'turma#update'
 end
