66958b814726f026bd7c1e2cfa85c5dac568f038
Thiago <tlcrezende@gmail.com>
2023-03-23 15:33:36 -0300

    update user model and controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,34 +1,44 @@
 class UserController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_user, only: %i[show update destroy]
+  before_action :authenticate_user!, except: :create
 
   def index
-    users = User.all
-    render json: users, status: 200
-  end
-
-  def create
-    User.create!(params.permit(:telefone, :password, :apelido))
+    @users = User.all
+    render json: @users
   end
 
   def show
-    if params['id'].present?
-      user = User.find(params['id'])
-      render json: user, status: 200 unless user.nil?
-      render json: { msg: 'usuário não encontrado' }, status: 404 if user.nil?
-    else
-      render json: { msg: 'parametro id faltando' }, status: 404
-    end
+    render json: @user
+  end
+
+  def create
+    byebug
+    @user = User.create!(params.permit(:telefone, :password, :apelido))
+    # render json: @user, status: :created if @user.save
+    # render json: @user.errors, status: :unprocessable_entity unless @user.save
   end
 
   def update
-    current_user.update(
-      params.permit(
-        :name, :data_nascimento, :email, :tipo_sanguineo, :tamanho_camisa, :tamanho_pe, :desconto,
-        :contato_emergencia_nome, :contato_emergencia_telefone, :responsavel_nome, :responsavel_parentesco,
-        :responsavel_telefone, :endereco, :cep, :foto_id, :data_inicio, :alergias, :expo_push_token, :vencimento
-      )
-    )
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
-    render json: 'usuário atualizado', status: 200
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:name, :data_nascimento, :email, :tipo_sanguineo, :tamanho_camisa, :tamanho_pe, :desconto,
+                  :contato_emergencia_nome, :contato_emergencia_telefone, :responsavel_nome, :responsavel_parentesco,
+                  :responsavel_telefone, :endereco, :cep, :foto_id, :data_inicio, :alergias, :expo_push_token, :vencimento)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise  :database_authenticatable, 
+  devise  :database_authenticatable,
           :registerable,
-          # :recoverable, 
-          # :rememberable, 
-          :validatable, 
+          # :recoverable,
+          # :rememberable,
+          :validatable,
           # :trackable,
           authentication_keys: [:telefone]
   include DeviseTokenAuth::Concerns::User
 
   validates :telefone, presence: true, uniqueness: true
+
+  has_many :pagamento
 
   before_validation do
     self.uid = telefone if uid.blank?
