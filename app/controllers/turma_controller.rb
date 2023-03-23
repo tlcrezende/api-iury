af65f82255a3c6b89f6d48f348bb2a5f7e4bb3a5
Thiago <tlcrezende@gmail.com>
2023-03-23 15:34:04 -0300

    fix turma endepoints

--- app/controllers/turma_controller.rb
+++ app/controllers/turma_controller.rb
@@ -1,34 +1,45 @@
 class TurmaController < ApplicationController
+  before_action :set_turma, only: %i[show update destroy]
 
+  def index
+    turmas = Turma.all
+    render json: turmas, status: 200
+  end
 
-    def index
-        turmas = Turma.all
-        render json: turmas, status: 200
+  def show
+    if params[:id]
+      user = Turma.find(params[:id])
+      render json: user, status: 200 unless user.nil?
+      render json: { msg: 'usuário não encontrado' }, status: 404 if user.nil?
+    else
+      render json: { msg: 'parametro id faltando' }, status: 404
     end
+  end
 
-    def show 
-        if params['id'].present?
-            user = Turma.find(params['id'])
-            render json: user, status: 200 unless user.nil?
-            render json: {msg: 'usuário não encontrado'}, status: 404 if user.nil?
-        else
-            render json: {msg: 'parametro id faltando'}, status: 404
-        end
+  def create
+    Turma.create!(params.permit(:sede, :dia, :horario, :professor, :valor))
+    render json: 'turma criada com sucesso', status: 200
+  end
+
+  def update
+    if @turma.update(turma_params)
+      render json: @turma
+    else
+      render json: @turma.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-        turma = Turma.new 
+  def destroy
+    @turma.destroy
+  end
 
-        turma.sede = params['sede'] if params['sede'].present?
-        turma.dia = params['dia'] if params['dia'].present?
-        turma.horario = params['horario'] if params['horario'].present?
-        turma.professor = params['professor'] if params['professor'].present?
-        turma.valor = params['valor'] if params['valor'].present?
+  private
 
-        if turma.save 
-            render json: 'turma criada com sucesso', status: 200 
-        else
-            render json: 'turma não criada', status: 400 
-        end
-    end
+  def set_turma
+    @turma = Turma.find(params[:id])
+  end
+
+  def turma_params
+    params.permit(:id, :sede, :dia, :horario, :professor, :valor)
+  end
 end
--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,3 +1,4 @@
 class Turma < ApplicationRecord
     validates :sede, :dia, :horario, :valor, :professor, presence: true 
+    has_many :pagamento
 end
